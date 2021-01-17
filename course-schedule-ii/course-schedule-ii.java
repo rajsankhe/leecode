873a3923081ca8371e6f3a39f58ddab6e476ea87
Raj Sankhe <rajsankhe03@gmail.com>
2021-01-16 21:09:21 -0600

    Time: 9 ms (29.61%), Space: 47 MB (7.22%) - LeetHub

---
+++ course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int courseOrder[] = new int[numCourses];
+        int indegree[] = new int[numCourses];
+        Map<Integer, List<Integer>> graph = buildGraph(numCourses, prerequisites, indegree);
+        int completedCourses = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        int k=0;
+        for(int i=0; i < numCourses; i++){
+            if (indegree[i] == 0) {
+                completedCourses++;
+                queue.add(i);
+                courseOrder[k++] = i;
+            }
+        }
+        
+        while(!queue.isEmpty()){
+            int pre = queue.poll();
+            if (graph.get(pre) != null){
+                for(int course : graph.get(pre)) {
+                indegree[course]--;
+                if (indegree[course] == 0){
+                    queue.add(course);
+                    completedCourses++;
+                    courseOrder[k++] = course;
+                }
+            }
+            }
+        }
+        return completedCourses == numCourses ? courseOrder : new int[0];
+    }
+    
+    public Map<Integer, List<Integer>> buildGraph(int numCourses, int[][] prerequisites, int indegree[]){
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for(int[] link : prerequisites){
+            if(!graph.containsKey(link[1]))
+                graph.put(link[1], new ArrayList<>());
+            graph.get(link[1]).add(link[0]);
+            indegree[link[0]]++;
+        }
+        return graph;
+    }
+}
