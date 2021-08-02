3a020c521577568f37c7eddf13e30e72e3130964
Raj Sankhe <rajsankhe03@gmail.com>
2021-08-02 12:34:09 -0500

    Time: 25 ms (66.98%), Space: 39.2 MB (68.61%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.java
+++ longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,31 +1,28 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if(s == null || s.length() < 1) return "";
-        
-        int start = 0, end = 0;
-        for(int i = 0; i< s.length(); i++){
-            int len1 = expandFromMiddle(s, i, i);
-            int len2 = expandFromMiddle(s,i, i+1);
-            
-            int len = Math.max(len1, len2);
-            
-            if(len > end-start){
-                start = i - (len - 1)/2;
-                end = i + (len/2);
-            }
-            
+        if (s == null || s.isEmpty()) {
+            return s;
         }
-         return s.substring(start, end+1);
+        
+        int[] globalMax = new int[2];
+        
+        for (int i =0; i<s.length(); i++) {
+            int[] r1= expandAroundCenter(s, i, i);
+            int[] r2= expandAroundCenter(s, i, i+1);
+            
+            int[] localMax= r1[1] -r1[0] > r2[1] - r2[0] ? r1 : r2;
+            globalMax = localMax[1] - localMax[0]  > globalMax[1] - globalMax[0]? localMax : globalMax;
+        }
+        
+        return s.substring(globalMax[0], globalMax[1] + 1);
     }
     
-    private int expandFromMiddle(String s, int left, int right){
-        if(s == null || left > right) return 0;
-        
-        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+    private int[] expandAroundCenter(String s, int left, int right){
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
             left--;
-            right ++;
+            right++;
         }
         
-        return right - left - 1; 
+        return new int[]{left + 1, right - 1};
     }
 }
