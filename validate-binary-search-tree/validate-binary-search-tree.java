9a93b74db06121e11e35d7d87a00e5a612a75826
Raj Sankhe <rajsankhe03@gmail.com>
2021-08-07 19:44:02 -0500

    Time: 0 ms (100.00%), Space: 38.7 MB (53.29%) - LeetHub

--- validate-binary-search-tree/validate-binary-search-tree.java
+++ validate-binary-search-tree/validate-binary-search-tree.java
@@ -1,30 +1,30 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    public boolean isValidBST(TreeNode root) {
-         return isValidBST(root,null, null);
-    }
-    
-    private boolean isValidBST(TreeNode root, Integer low, Integer high){
-        if (root == null)
-            return true;
-        
-        if ( (low != null && root.val <= low) || (high != null && root.val >= high))
-            return false;
-        
-        return isValidBST(root.left, low, root.val) && isValidBST(root.right, root.val, high);
-    }
+    Integer prev = null;
+    public boolean isValidBST(TreeNode root) {
+        if (root == null)
+            return true;
+        if (!isValidBST(root.left)){
+            return false;
+        } else if(prev != null && prev >= root.val){
+            return false;
+        }
+        
+        prev = root.val;
+        return isValidBST(root.right);
+    }
 }
