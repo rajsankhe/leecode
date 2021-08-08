1125b30ec3d69cb6c3cc3a8a4bf39c5da4e8e2c4
Raj Sankhe <rajsankhe03@gmail.com>
2021-08-08 09:53:11 -0500

    Time: 1 ms (73.61%), Space: 39.3 MB (19.50%) - LeetHub

--- binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -1,42 +1,46 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-                List<List<Integer>> result = new ArrayList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
-        boolean reverse = false;
-        if(root != null)
-            queue.add(root);
-        while(!queue.isEmpty()) {
-            int size = queue.size();
-            List<Integer> op = new ArrayList<>();
-            for (int i=0 ; i<size; i++) {
-                TreeNode node = queue.poll();
-                if(node.left != null)
-                    queue.add(node.left);
-                if(node.right != null)
-                    queue.add(node.right);
-                op.add(node.val);
-            }
-            if (reverse) {
-                Collections.reverse(op);
-            }
-            reverse = !reverse;
-            result.add(op);
-        }
-        return result;
-    }
+    
+    public void dfs(TreeNode node, int level,List<List<Integer>> result){
+        if(level >= result.size()){
+            List<Integer> levelList = new LinkedList<>();
+            levelList.add(node.val);
+            result.add(levelList);
+        } else{
+            if(level % 2 == 0){
+                result.get(level).add(node.val);
+            } else{
+                result.get(level).add(0, node.val);
+            }
+        }
+        if(node.left != null)
+        dfs(node.left, level+1, result);
+        if(node.right != null)
+        dfs(node.right, level+1, result);
+    }
+    
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        if(root == null){
+         return result;   
+        }
+        
+        dfs(root, 0, result);
+        return result;
+    }
 }
