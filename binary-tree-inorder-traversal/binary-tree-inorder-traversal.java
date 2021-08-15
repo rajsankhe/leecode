843d001aeb9ff3b83d6de3f6922ab5a4ad07675c
Raj Sankhe <rajsankhe03@gmail.com>
2021-08-15 17:29:50 -0500

    Time: 0 ms (100.00%), Space: 37.6 MB (20.83%) - LeetHub

--- binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -1,30 +1,36 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> output = new ArrayList<>();
-        inorderTraversal(root, output);
-        return output;
-    }
-    
-    public void inorderTraversal(TreeNode root, List<Integer> output){
-        if(root == null)
-            return;
-        inorderTraversal(root.left, output);
-        output.add(root.val);
-        inorderTraversal(root.right, output);
-    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> op = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack();
+        
+        while(root != null || !stack.isEmpty()){
+            
+            while(root != null){
+                stack.push(root);
+                root = root.left;
+            }
+            
+            TreeNode node = stack.pop();
+            op.add(node.val);
+            root = node.right;
+            
+        }
+        
+        return op;
+    }
 }
