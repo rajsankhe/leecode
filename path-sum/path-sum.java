2137bcd74ee88fb0304f746056a589e013d18c08
Raj Sankhe <rajsankhe03@gmail.com>
2021-10-16 15:44:56 -0500

    Time: 0 ms (100.00%), Space: 39.2 MB (47.60%) - LeetHub

--- path-sum/path-sum.java
+++ path-sum/path-sum.java
@@ -15,20 +15,16 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        return hasPathSum(root, 0, targetSum);
-    }
-    
-    public boolean hasPathSum(TreeNode node, int sum, int targetSum){
-        if(node == null){
+        if(root == null)
             return false;
+        
+        targetSum = targetSum - root.val;
+            
+        if(root.left == null && root.right == null){
+            return targetSum == 0;
         }
         
-        sum = sum + node.val;
+        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
         
-        if(sum == targetSum && node.left == null && node.right == null){
-            return true;
-        }
-        
-        return hasPathSum(node.left, sum, targetSum) || hasPathSum(node.right, sum, targetSum);
     }
 }
